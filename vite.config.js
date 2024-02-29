93450bae52a0d46fb1d2b554098b03b5ec5e325c
ernaaa <70594879+ernaaa@users.noreply.github.com>
2024-02-29 21:55:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,36 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    css: {
+      css: {
         preprocessorOptions: {
             scss: {
                 additionalData:`@import "@/scss/variables.scss";`
             }
         }
     },
-    plugins: [
-        vue()
-    ],
+    alias: {
+    "@": resolve(__dirname, "src"),
+  },
+  plugins: [vueJsx()],
+  build: {
+    rollupOptions: {
+      external: [
+        "react", // ignore react stuff
+        "react-dom",
+      ],
+    },
+  },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
+  optimizeDeps: {
+    include: ["@apollo/client/core", "@apollo/client/link/error"],
+  },
 })
