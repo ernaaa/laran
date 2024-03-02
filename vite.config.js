8a6175cad3f00ceaf3531b04268fd9c7d2b62d45
ernaaa <70594879+ernaaa@users.noreply.github.com>
2024-03-02 18:02:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,22 @@
-import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue';
-import { resolve } from "path";
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'url'
 
-// Exportieren der Konfiguration mit defineConfig für bessere Typunterstützung
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
-  plugins: [vue(), vueJsx()], // Hinzufügen von vue() zum Plugin-Array
-  resolve: {
-    alias: {
-      // Korrekte Verwendung von Alias
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData:`@import "@/scss/variables.scss";`
+            }
+        }
     },
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        "react", // Ignorieren von React
-        "react-dom",
-      ],
-    },
-  },
-  optimizeDeps: {
-    include: ["@apollo/client/core", "@apollo/client/link/error"],
-  },
-});
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+})
