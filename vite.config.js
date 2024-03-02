d580670696f94b110b9385f21bd12b5042fa878c
ernaaa <70594879+ernaaa@users.noreply.github.com>
2024-03-02 17:42:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ alias: {
   },
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@'; fileURLToPath(new URL('./src', import.meta.url))
         }
     }
 }
