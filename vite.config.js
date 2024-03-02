8f8354e525460ab1cd527043c81747501fc5e444
ernaaa <70594879+ernaaa@users.noreply.github.com>
2024-03-02 18:10:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,21 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData:`@import "@/scss/variables.scss";`
+                additionalData: `@import "@/scss/variables.scss";`
             }
         }
     },
     plugins: [
         vue()
     ],
-    build: {
-    rollupOptions: {
-      external: [
-        externalCssPath,
-      ],
-    },
-  },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
-})
+});
